484fa8df0e4382c049c89910fdae11c06aff1313
iyede <iyede@DESKTOP-Q7GSR7S>
2020-01-11 18:32:07 +0100

    Client Tests working

--- bankingSys/src/main/java/bankingSys/Client.java
+++ bankingSys/src/main/java/bankingSys/Client.java
@@ -1,26 +1,133 @@
 package bankingSys;
 
+import java.util.Date;
+
 public class Client {
 	private String firstName;
 	private String lastName;
+	private String adress;
+	private Date birthDate;
+	private String birthPlace;
+	private String status;
+	private String phoneNumber;
 	
 	
 	public Client(String name, String familyName) {
+		
+		if (name == null)
+			throw new IllegalArgumentException("Client First Name cant be null");
+		
+		if (familyName == null)
+			throw new IllegalArgumentException("Client Last Name cant be null");
+
+		if (name.length() == 0)
+			throw new IllegalArgumentException("Client First Name must contain at least one caracter");
+		
+		if (familyName.length() == 0)
+			throw new IllegalArgumentException("Client Last Name must contain at least one caracter");
+		
+		
 		firstName = name;
 		lastName = familyName;
-				
+		
 	}
 
 
-	public Object getFirstName() {
+	public String getFirstName() {
 		// TODO Auto-generated method stub
 		return firstName;
 	}
 
 
-	public Object getLastName() {
+	public String getLastName() {
 		// TODO Auto-generated method stub
 		return lastName;
 	}
+
+
+	public void setAdress(String myAdress) {
+		// TODO Auto-generated method stub
+		if (myAdress == null)
+			throw new IllegalArgumentException("User name must contain at least one caracter");
+		else if (myAdress.length() == 0)
+			throw new IllegalArgumentException("User name must contain at least one caracter");
+		else
+			adress = myAdress;
+
+	}
+
+
+	public String getAdress() {
+		// TODO Auto-generated method stub
+		return adress;
+	}
+
+
+	public void setBirthDate(Date myBirthday) {
+		// TODO Auto-generated method stub
+		if (myBirthday == null)
+			throw new IllegalArgumentException("Birthday parameter cannot be null");
+		else
+			birthDate = myBirthday;
+	}
+
+
+	public Date getBirthDate() {
+		// TODO Auto-generated method stub
+		return birthDate;
+	}
+
+
+	public void setBirthPlace(String myBirthPlace) {
+		// TODO Auto-generated method stub
+		if (myBirthPlace == null)
+			throw new IllegalArgumentException("User name must contain at least one caracter");
+		else if (myBirthPlace.length() == 0)
+			throw new IllegalArgumentException("User name must contain at least one caracter");
+		else
+			birthPlace = myBirthPlace;
+
+	}
+
+
+	public String getBirthPlace() {
+		// TODO Auto-generated method stub
+		return birthPlace;
+	}
+
+
+	public void setStatus(String myStatus) {
+		// TODO Auto-generated method stub
+		if (myStatus == null)
+			throw new IllegalArgumentException("User name must contain at least one caracter");
+		else if (myStatus.length() == 0)
+			throw new IllegalArgumentException("User name must contain at least one caracter");
+		else
+			status = myStatus;
+	}
+
+
+	public String getStatus() {
+		// TODO Auto-generated method stub
+		return status;
+	}
+
+
+	public void setPhoneNumber(String myPhoneNumber) {
+		// TODO Auto-generated method stub
+		if (myPhoneNumber == null)
+			throw new IllegalArgumentException("User name must contain at least one caracter");
+		else if (myPhoneNumber.length() == 0)
+			throw new IllegalArgumentException("User name must contain at least one caracter");
+		else
+			phoneNumber = myPhoneNumber;
+	}
+
+
+	public String getPhoneNumber() {
+		// TODO Auto-generated method stub
+		return phoneNumber;
+	}
+
 
 }
--- bankingSys/src/test/java/bankingSys/clientTest.java
+++ bankingSys/src/test/java/bankingSys/clientTest.java
@@ -4,6 +4,8 @@ package bankingSys;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.Date;
+import java.util.Calendar;
 
 
 
@@ -33,7 +35,7 @@ class clientTest {
 		String firstName = "John";
 		String lastName = null;
 		
-		assertThrows(NullPointerException.class, () -> {
+		assertThrows(IllegalArgumentException.class, () -> {
 			new Client(firstName, lastName);		
 		});
 	
@@ -46,7 +48,7 @@ class clientTest {
 		String lastName = "DOE";
 		
 		
-		assertThrows(NullPointerException.class, () -> {
+		assertThrows(IllegalArgumentException.class, () -> {
 			new Client(firstName, lastName);		
 		});
 	}
@@ -59,7 +61,7 @@ class clientTest {
 		String lastName = "";
 		
 		
-		assertThrows(NullPointerException.class, () -> {
+		assertThrows(IllegalArgumentException.class, () -> {
 			new Client(firstName, lastName);		
 		});
 	}
@@ -71,13 +73,115 @@ class clientTest {
 		String lastName = "DOE";
 		
 		
-		assertThrows(NullPointerException.class, () -> {
+		assertThrows(IllegalArgumentException.class, () -> {
 			new Client(firstName, lastName);		
 		});
 	}
 	
+	@Test
+	public void ClientAdressVerificationProper() {
+		
+		String firstName = "John";
+		String lastName = "DOE";
+		
+		//Proper client creation and instanciation only needs name and last name
+		Client testClient = new Client(firstName, lastName);
+		
+		assertNotNull(testClient);
+		//because you can never be too careful
+
+		String adress = "13 rue de France";
+		testClient.setAdress(adress);
+		
+		assertEquals( testClient.getAdress(), adress);
+	
+	}
 	
 	
+	@Test
+	public void ClientBirthDateVerificationProper() {
+		
+		String firstName = "John";
+		String lastName = "DOE";
+		
+		//Proper client creation and instanciation only needs name and last name
+		Client testClient = new Client(firstName, lastName);
+		
+		assertNotNull(testClient);
+		//because you can never be too careful
+
+		
+        Calendar c1 = Calendar.getInstance(); 
+        c1.set(Calendar.MONTH, 11); 
+        c1.set(Calendar.DATE, 21); 
+        c1.set(Calendar.YEAR, 1993); 
+  
+        // creating a date object with specified time. 
+        Date birthday = c1.getTime(); 
+		
+		testClient.setBirthDate(birthday);
+		
+		assertEquals( testClient.getBirthDate(), birthday);
+	
+	}
+	
+	@Test
+	public void ClientBirthPlaceVerificationProper() {
+		
+		String firstName = "John";
+		String lastName = "DOE";
+		
+		//Proper client creation and instanciation only needs name and last name
+		Client testClient = new Client(firstName, lastName);
+		
+		assertNotNull(testClient);
+		//because you can never be too careful
+
+		String birthPlace = "Paris";
+		testClient.setBirthPlace(birthPlace);
+		
+		assertEquals( testClient.getBirthPlace(), birthPlace);
+	
+	}
+	
+	
+	@Test
+	public void ClientStatusVerificationProper() {
+		
+		String firstName = "John";
+		String lastName = "DOE";
+		
+		//Proper client creation and instanciation only needs name and last name
+		Client testClient = new Client(firstName, lastName);
+		
+		assertNotNull(testClient);
+		//because you can never be too careful
+
+		String status = "Engineer";
+		testClient.setStatus(status);
+		
+		assertEquals( testClient.getStatus(), status);
+	
+	}
+	
+	@Test
+	public void ClientPhoneNumberVerificationProper() {
+		
+		String firstName = "John";
+		String lastName = "DOE";
+		
+		//Proper client creation and instanciation only needs name and last name
+		Client testClient = new Client(firstName, lastName);
+		
+		assertNotNull(testClient);
+		//because you can never be too careful
+
+		String phoneNumber = "01 11 11 11 11";
+		testClient.setPhoneNumber(phoneNumber);
+		
+		assertEquals( testClient.getPhoneNumber(), phoneNumber);
+	
+	}
 	
 	
 
